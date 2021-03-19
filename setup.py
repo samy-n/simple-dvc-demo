6e546bebe7677604947427f3247bdcc5a2a8783c
Samyukta Nair <samycode00@gmail.com>
2021-03-19 21:47:15 +0530

    setup tox update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="wine q package",
+    author="samy-n",
+    packages=find_packages(),
+    license="MIT"
+)
